fbf4bb1a13e9ebbe9b111eab12de3b1e268a596b
sophie5612 <49294529+sophie5612@users.noreply.github.com>
2023-04-29 12:38:47 -0400

    More Gradle fixes

--- app/src/main/java/com/example/imbored/ui/gallery/Datasource.kt
+++ app/src/main/java/com/example/imbored/ui/gallery/Datasource.kt
@@ -1,22 +1,22 @@
-package com.example.imbored.ui.gallery
-
-import com.example.imbored.R
-
-class Datasource() {
-
-    fun loadActivityPhotos (): List<ActivityPhoto> {
-        return listOf<ActivityPhoto>(
-            ActivityPhoto(R.string.affirmation1, R.drawable.image1),
-            ActivityPhoto(R.string.affirmation2, R.drawable.image2),
-            ActivityPhoto(R.string.affirmation3, R.drawable.image3),
-            ActivityPhoto(R.string.affirmation4, R.drawable.image4),
-            ActivityPhoto(R.string.affirmation5, R.drawable.image5),
-            ActivityPhoto(R.string.affirmation6, R.drawable.image6),
-            ActivityPhoto(R.string.affirmation7, R.drawable.image7),
-            ActivityPhoto(R.string.affirmation8, R.drawable.image8),
-            ActivityPhoto(R.string.affirmation9, R.drawable.image9),
-            ActivityPhoto(R.string.affirmation10, R.drawable.image10)
-        )
-    }
-
-}
+//package com.example.imbored.ui.gallery
+//
+//import com.example.imbored.R
+//
+//class Datasource() {
+//
+//    fun loadActivityPhotos (): List<ActivityPhoto> {
+//        return listOf<ActivityPhoto>(
+//            ActivityPhoto(R.string.affirmation1, R.drawable.image1),
+//            ActivityPhoto(R.string.affirmation2, R.drawable.image2),
+//            ActivityPhoto(R.string.affirmation3, R.drawable.image3),
+//            ActivityPhoto(R.string.affirmation4, R.drawable.image4),
+//            ActivityPhoto(R.string.affirmation5, R.drawable.image5),
+//            ActivityPhoto(R.string.affirmation6, R.drawable.image6),
+//            ActivityPhoto(R.string.affirmation7, R.drawable.image7),
+//            ActivityPhoto(R.string.affirmation8, R.drawable.image8),
+//            ActivityPhoto(R.string.affirmation9, R.drawable.image9),
+//            ActivityPhoto(R.string.affirmation10, R.drawable.image10)
+//        )
+//    }
+//
+//}
--- app/src/main/java/com/example/imbored/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/imbored/ui/gallery/GalleryFragment.kt
@@ -1,49 +1,49 @@
-package com.example.imbored.ui.gallery
-
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
-import com.example.imbored.R
-import com.example.imbored.databinding.FragmentGalleryBinding
-
-class GalleryFragment : Fragment() {
-
-    private var _binding: FragmentGalleryBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val galleryViewModel =
-           ViewModelProvider(this).get(GalleryViewModel::class.java)
-
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val myDataset = Datasource().loadAffirmations()
-        val recyclerView: RecyclerView = requireView().findViewById(R.id.recycler_view)
-        recyclerView.adapter = ItemAdapter(this, myDataset)
-        recyclerView.setHasFixedSize(true)
-
-        //overrides text
-        //val textView: TextView = binding.textView4
-       // galleryViewModel.text.observe(viewLifecycleOwner) {
-       //     textView.text = it
-       // }
-        return root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
-}
+//package com.example.imbored.ui.gallery
+//
+//import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.fragment.app.Fragment
+//import androidx.lifecycle.ViewModelProvider
+//import androidx.recyclerview.widget.RecyclerView
+//import com.example.imbored.R
+//import com.example.imbored.databinding.FragmentGalleryBinding
+//
+//class GalleryFragment : Fragment() {
+//
+//    private var _binding: FragmentGalleryBinding? = null
+//
+//    // This property is only valid between onCreateView and
+//    // onDestroyView.
+//    private val binding get() = _binding!!
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        val galleryViewModel =
+//           ViewModelProvider(this).get(GalleryViewModel::class.java)
+//
+//        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+//        val root: View = binding.root
+//
+//        val myDataset = Datasource().loadAffirmations()
+//        val recyclerView: RecyclerView = requireView().findViewById(R.id.recycler_view)
+//        recyclerView.adapter = ItemAdapter(this, myDataset)
+//        recyclerView.setHasFixedSize(true)
+//
+//        //overrides text
+//        //val textView: TextView = binding.textView4
+//       // galleryViewModel.text.observe(viewLifecycleOwner) {
+//       //     textView.text = it
+//       // }
+//        return root
+//    }
+//
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
+//}
