2a92fb641ca71a0d13e4d06d875c869574ecb230
Katelyn Wyandt <kat.wyandt.812@gmail.com>
2023-04-30 12:10:25 -0400

    me when i realize i need to do a camera activity not a fragment

--- app/src/main/java/com/example/imbored/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/imbored/ui/gallery/GalleryFragment.kt
@@ -9,12 +9,16 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.commit
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.imbored.CameraFragment
 import com.example.imbored.HomeViewModel
+import com.example.imbored.R
 import com.example.imbored.databinding.FragmentGalleryBinding
 
 class GalleryFragment : Fragment() {
@@ -36,8 +40,22 @@ class GalleryFragment : Fragment() {
         //UI Bound Object
 
         val cameraView: ConstraintLayout = binding.cameraButton
+        //not sure if i should use child or parent
+        //val fragmentManager = parentFragmentManager
+        //pressing camera button opens up the camera activtiy
         cameraView.setOnClickListener{
             //FragmentTransaction
+            TODO("Implement camera activity, delete fragments")
+            val cameraFragment = CameraFragment()
+            //fragmentManager.beginTransaction()
+            //    .replace(this, cameraFragment)
+            //    .addToBackStack(null)
+           //     .commit()
+            //childFragmentManager.commit{
+           //     replace(R.id.fragment_container_view, cameraFragment)
+            //setReorderingAllowed(true)
+            //    addToBackStack("GalleryFragment")
+            //}
         }
 
         return binding.root
@@ -56,4 +74,13 @@ class GalleryFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+}
+
+//i think i no longer need this
+
+private fun FragmentTransaction.replace(
+    galleryFragment: GalleryFragment,
+    cameraFragment: CameraFragment
+): FragmentTransaction {
+    TODO("Not yet implemented")
 }
